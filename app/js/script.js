9d6e8e55613695d93149e5ea081ba8aae21aeeb7
Bruno Sequeira <sequeirab86@gmail.com>
2021-01-19 17:07:50 +0000

    checkpoint

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,6 @@
-console.log('Hello World');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+
+btnHamburger.addEventListener('click', function() {
+    header.classList.toggle('open');
+})
